f0ae2f7ae95a26605ec701d901904b8d25c5b06f
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-07-17 04:04:31 +0530

    Made some changes

--- terraform-local-file/local.tf
+++ terraform-local-file/local.tf
@@ -1,16 +1,16 @@
 resource "local_file" "pet" {
   filename        = var.filename
-  content         = var.content
+  content         = var.content["statement1"]
   file_permission = "0700"
 }
 
 resource "local_file" "cat" {
   filename = var.filename
-  content  = var.content
+  content  = var.content["statement2"]
 }
 
 resource "random_pet" "my-pet" {
-  prefix    = var.prefix
+  prefix    = var.prefix[2]
   separator = var.separator
   length    = var.length
 }
--- terraform-local-file/variables.tf
+++ terraform-local-file/variables.tf
@@ -1,15 +1,31 @@
 variable "filename" {
-  default = "./pets.txt"
+  default     = "./pets.txt"
+  type        = string
+  description = "The path of Local File"
 }
 variable "content" {
-  default = "We love Pets!"
+  type        = map(string)
+  description = "The content of Local File"
+  default = {
+    "statement1" = "We love Pets!"
+    "statement2" = "We love Cats!"
+    "statement3" = "We love Animals!  "
+  }
 }
 variable "prefix" {
-  default = "Mrs"
+  default     = ["Mrs", "Mr", "sir", "Miss"]
+  type        = list(string)
+  description = "The prefix to be set"
 }
 variable "separator" {
   default = "."
 }
 variable "length" {
-  default = 3
+  default     = 1
+  type        = number
+  description = "The length of the prefix to be set"
+}
+variable "password_change" {
+  default = true
+  type    = bool
 }

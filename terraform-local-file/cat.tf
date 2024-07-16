8fe7392c9e674688d4d790ac5b38896672343ee1
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-07-17 03:27:56 +0530

    made some changes in some files

---
+++ terraform-local-file/cat.tf
@@ -0,0 +1,4 @@
+# resource "local_file" "cat" {
+#     filename = "./cat.txt"
+#     content = "My Favourite pet is Mr.Whiskers"
+# }
--- terraform-local-file/local.tf
+++ terraform-local-file/local.tf
@@ -1,4 +1,16 @@
 resource "local_file" "pet" {
-  filename = "./pets.txt"
-  content  = "We love Pets!"
+  filename        = "./pets1.txt"
+  content         = "We love Pets!"
+  file_permission = "0700"
+}
+
+resource "local_file" "cat" {
+  filename = "./cat.txt"
+  content  = "My Favourite pet is Mr.Whiskers"
+}
+
+resource "random_pet" "my-pet" {
+  prefix    = "Mrs"
+  separator = "."
+  length    = "1"
 }
